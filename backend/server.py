9933d9cfa457e61b212c441c6f715778d646a002
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-10-28 00:09:35 +0200

    Initial Commit

---
+++ backend/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
+
+from pymongo import MongoClient
+
+app = Flask(__name__)
+CORS(app)
+
+
+client = MongoClient('mongodb', 27017)
+
+db = client.pymongo_test
+
+
+@app.route('/')
+def index():
+    posts = db.posts
+    post_data = {
+        'title': 'Python and MongoDB',
+        'content': 'PyMongo is fun, you guys',
+        'author': 'Scott'
+    }
+    result = posts.insert_one(post_data)
+    print('One post: {0}'.format(result.inserted_id))
+
+    return jsonify(str(result.inserted_id))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
